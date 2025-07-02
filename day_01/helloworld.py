94bdb8beeabe260a53f61cf555946c5b827f1673
Benjamin-Chong <ben.chong0248@gmail.com>
2025-07-02 16:54:14 -0700

    Day 1: Introduction
    Covered python interactive shell, operations, and calculated distance using the Euclidian distance formula.

---
+++ day_01/helloworld.py
@@ -0,0 +1,21 @@
+#Day 1
+
+print(2 + 3)
+print(3 - 1)
+print(2 * 3)
+print(3 / 2)
+print(3 ** 2)
+print(3 % 2)
+print(3 // 2)
+
+print(type(10))
+print(type(3.14))
+print(type(1 + 3j))
+print(type('Benjamin'))
+print(type([1, 2, 3]))
+print(type({'name':'Benjamin'}))
+print(type((9.8, 3.14, 2.7)))
+print(type({9.8, 3.14, 2.7}))
+
+
+
